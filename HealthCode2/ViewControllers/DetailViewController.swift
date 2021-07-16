9e8698dd4f59895cae43cb98e35946f8c0220ef6
Nicole Mellert <nmellert@nd.edu>
2021-07-16 16:51:38 -0400

    Added byline and abstract label to DetailViewController with pulled details

--- HealthCode2/ViewControllers/DetailViewController.swift
+++ HealthCode2/ViewControllers/DetailViewController.swift
@@ -11,13 +11,22 @@ class DetailViewController: UIViewController {
     
     @IBOutlet weak var articleTitle: UILabel!
 
+    @IBOutlet weak var authorByLine: UILabel!
+    
+    @IBOutlet weak var abstract: UILabel!
+    
     var myArticle = [String:Any?]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.articleTitle.text = self.myArticle["title"] as? String
+        self.authorByLine.text = self.myArticle["byline"] as? String
+        self.abstract.text = self.myArticle["abstract"] as? String
+
+
     }
+    
     
 
 }
