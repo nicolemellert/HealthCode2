9d7f18c5c0a3c543930b43717f4cc5779470fe09
Nicole Mellert <nmellert@nd.edu>
2021-07-26 09:08:56 -0400

    Added constraints to labels and imageView

--- HealthCode2/ViewControllers/DetailViewController.swift
+++ HealthCode2/ViewControllers/DetailViewController.swift
@@ -23,21 +23,6 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-            // Create URL - random URL but eventually fill from myArticle data
-        let url = URL(string: "https://cdn.cocoacasts.com/cc00ceb0c6bff0d536f25454d50223875d5c79f1/above-the-clouds.jpg")!
-
-         // THIS CODE RUNS BUT not entirely as it should
-         let dataTask = URLSession.shared.dataTask(with: url) { [weak self] (data, _, _) in
-             if let data = data {
-                 // Create Image and Update Image View
-                 self?.ImagePreview.image = UIImage(data: data)
-             }
-         }
-         
-
-         // Start Data Task
-         dataTask.resume()
-        
         self.articleTitle.text = self.myArticle["title"] as? String
         self.authorByLine.text = self.myArticle["byline"] as? String
         self.abstract.text = self.myArticle["abstract"] as? String
@@ -78,18 +63,3 @@ extension UIImageView{
     
 }
 
-
-
-//extension UIImageView {
-//    func load(url: URL) {
-//        DispatchQueue.global().async { [weak self] in
-//            if let data = try? Data(from: url as! Decoder) as? Foundation.Data {
-//                if let image = UIImage(data: data){
-//                    DispatchQueue.main.async {
-//                        self?.image = image
-//                    }
-//                }
-//            }
-//        }
-//    }
-//}
